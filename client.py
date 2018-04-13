49838f5eb4d739f70bbdc114e5c4709489bd958b
LoïcGoulefert <loic.goulefert31@gmail.com>
2018-04-13 11:16:09 +0200

    Envoie le nom du fichier de config

--- client.py
+++ client.py
@@ -5,12 +5,13 @@ from socket import *
 """The client just sends messages to the server."""
 
 def send_data(messages):
+	"""Connect to the server, then send each string in the array "messages" to the server."""
 	host = ('127.0.0.2', 5000)
 	s = socket(AF_INET, SOCK_STREAM)
 	s.connect(host)
 
-	# Faut d'abord envoyer le nb de trames qu'on va envoyer
 	for m in messages:
+		#Add size checking (< 2048), or maybe the planner will do this
 		print('Sending : ' + m)
 		s.send(m.encode())
 		answer = s.recv(2048).decode()
@@ -21,6 +22,7 @@ def send_data(messages):
 
 if __name__ == '__main__':
 	messages = ['#1a 1 1,c 2 3', '#2b 0 4', '#3A 0 0,B 4 4', \
-	'#4A DOWN,B UP,A RIGHT,B LEFT,B UP,A UP,A RIGHT,B UP,B RIGHT,B UP,B LEFT,A RIGHT']
+	'#4A DOWN,B UP,A RIGHT,B LEFT,B UP,A UP,A RIGHT,B UP,B RIGHT,B UP,B LEFT,A RIGHT',  \
+	'#4B LEFT,B LEFT,B DOWN,B RIGHT,B DOWN', '#cm1.txt']
 	send_data(messages)
 
