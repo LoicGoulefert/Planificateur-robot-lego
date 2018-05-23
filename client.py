0668d492dbba61fb152961fa7216a1d99f61e82a
LoïcGoulefert <loic.goulefert31@gmail.com>
2018-05-23 11:26:50 +0200

    Clean client + doc

--- client.py
+++ client.py
@@ -3,9 +3,9 @@
 # Libs
 from socket import socket, AF_INET, SOCK_STREAM
 import pickle
-"""The client just sends messages to the server.
+"""The client just sends messages or objects to the server.
 
-Packets identifiers (first 2 char of each message):
+Packets identifiers for messages(first 2 char of each message):
 #c -> config file name (must be the same on both sides)
 #1 -> coord. objectives
 #2 -> coord. static objectives
@@ -20,6 +20,12 @@ CHUNK_SIZE = 4096  # Maximum message size the server can receive
 def send_data(messages, IPAdr='127.0.0.2', port=5000):
     """Connect to the server, then send each string
     in the array "messages" to the server.
+    Close the socket when it's done.
+
+    Parameters:
+        messages: list of strings, the messages to be sent
+        IPAdr: string, IP of the server
+        port: int, port we're emitting on
     """
     host = (IPAdr, port)
     s = socket(AF_INET, SOCK_STREAM)
@@ -35,6 +41,14 @@ def send_data(messages, IPAdr='127.0.0.2', port=5000):
 
 
 def send_object(obj, IPAdr='127.0.0.2', port=5000):
+    """Send a pickled object to the server.
+    Close the socket when it's done.
+
+    Parameters:
+        obj: The object to be sent
+        IPAdr: string, IP of the server
+        port: int, port we're emitting on
+    """
     host = (IPAdr, port)
     s = socket(AF_INET, SOCK_STREAM)
     s.connect(host)
--- planner.py
+++ planner.py
@@ -14,21 +14,30 @@ from bitvector import convert_to_bv, set_robot_list
 
 """How it works :
 1) Config of pddl files + IP, port
-2) Get info from pddl files
-3) Convert these into bitvectors
-4) Search for a solution in state graph
-5) Parse solution to send it to the simulator
+2) Get info from pddl files (domprob, init_state...)
+3) Build and send conf_list to simulator
+4) Convert these infos into bitvectors
+5) Search for a solution in state graph
+6) Parse solution to send it to the simulator
 """
 
 
 def get_domprob(domain_path, problem_path):
-    """Returns the domain problem"""
+    """Returns the DomainProblem object from pddlpy.
+
+    Parameters:
+        domain_path: string, path of the domain file
+        problem_path: string, path of the problem file
+    """
     return pddlpy.DomainProblem(domain_path, problem_path)
 
 
 def get_nb_robots(initial_state):
     """Returns the number of robots in the maze.
     This function is specific to maze planning problems.
+
+    Parameters:
+        initial_state: set of tuples ('at', 'X', 'c-0-0')
     """
     res = 0
     for s in initial_state:
@@ -40,6 +49,9 @@ def get_nb_robots(initial_state):
 def get_robot_list(initial_state):
     """Returns a list containing the names of the robots,
     sorted alphabetically.
+
+    Parameters:
+        initial_state: set of tuples ('at', 'X', 'c-0-0')
     """
     res = list()
     for s in initial_state:
@@ -49,23 +61,14 @@ def get_robot_list(initial_state):
     return res
 
 
-# def build_config_list(initial_state_bv, ground_op_bv):
-#     """Build the list of all preconditions that can be true.
-
-#     Parameters:
-#         initial_state_bv: set of bitvectors
-#         ground_op_bv: list of sets of GroundOpBV
-#     """
-#     res = list()
-#     res.append(initial_state_bv)
-#     for gopset in ground_op_bv:
-#         for gop in gopset:
-#             if gop.effect_pos not in res:
-#                 res.append(gop.effect_pos)
-#     return res
-
 def build_config_list(initial_state, op_list, domprob):
-    # Version avec les tuples
+    """Build the list of all preconditions that can be true.
+
+    Parameters:
+        initial_state: set of tuples ('at', 'X', 'c-0-0')
+        op_list: list of strings, the names of the operators
+        domprob: DomainProblem object from pddlpy
+    """
     res = initial_state.copy()
     for op in op_list:
         ground_op = domprob.ground_operator(op)
@@ -75,11 +78,14 @@ def build_config_list(initial_state, op_list, domprob):
     return list(res)
 
 
-def get_width_and_height(pddl_problem_file):
+def get_width_and_height(problem_file):
     """Returns the width and height of the maze.
     This function is specific to maze planning problems.
+
+    Parameters:
+        problem_file: string, path to pddl problem file
     """
-    with open(pddl_problem_file) as f:
+    with open(problem_file) as f:
         first_line = f.readline()
         second_line = f.readline()
         width = int(first_line.strip('; \n'))
@@ -101,58 +107,64 @@ def configure_planner():
     return domain, problem, IPAdr, port
 
 
+def search(f, params, timer=True):
+    """Search for a solution using 'f' function.
+    Returns a path if a solution is found, else None.
+
+    Parameters:
+        f : search function name
+        params: a tuple of parameters
+    """
+    t0 = time()
+    path = f(*params)
+    if timer:
+        print('Search duration: {}s'.format(time() - t0))
+    print('Number of nodes explored: {}'.format(len(Node.all_children)))
+    return path
+
+
 def main():
     # domain, problem, IPAdr, port = configure_planner()
-    maze_pb = int(input('Quel fichier de probleme ? (0,1,2,3 ou 4) :'))
+    maze_pb = int(input('Wich problem file should we use ?'
+                        ' (0,1,2,3 ou 4) :'))
     if maze_pb == 3 or maze_pb == 4:
         file = 'm1.txt'
     else:
         file = 'test.txt'
+
+    # Loading PDDL datas
     problem_file = 'pddl/problem-maze{}.pddl'.format(maze_pb)
     domprob = get_domprob('pddl/domain-maze.pddl', problem_file)
     goal = convert_to_tuple_set(domprob.goals())
     initial_state = convert_to_tuple_set(domprob.initialstate())
     op_list = list(domprob.operators())
-    # conf_list = build_config_list(initial_state, op_list, domprob)
-    # print(len(conf_list))
-    # print(conf_list)
-    # input()
 
-    # ********************Test BV********************************
+    # Init bitvectors
     nb_robots = get_nb_robots(initial_state)
     width, height = get_width_and_height(problem_file)
     set_robot_list(initial_state)
     init_bv = convert_to_bv(initial_state, nb_robots, width, height)
     goal_bv = convert_to_bv(goal, nb_robots, width, height)
-
-    conf_list = build_config_list(initial_state, op_list, domprob)
-
-    # ***********************************************************
-
     root = create_root(init_bv)
-    t0 = time()
-    path = dijkstra_search(
-       root, goal_bv, init_bv, domprob, nb_robots, width, height)
-    t1 = time()
-    print('dijkstra_search : {}s'.format(t1 - t0))
+    params = (root, goal_bv, init_bv, domprob, nb_robots, width, height)
 
-    # t0 = time()
-    # path = a_star_search(
-    #     root, goal_bv, init_bv, domprob, nb_robots, width, height)
-    # t1 = time()
-    # print("a_star_search : {}s".format(t1 - t0))
-    print('Number of nodes explored: {}'.format(len(Node.all_children)))
+    # Creating and sending config list
+    conf_list = build_config_list(initial_state, op_list, domprob)
+    send_object(conf_list)
+
+    # Searching for a path
+    path = search(dijkstra_search, params)
 
     if path is None:
         print('No path found.')
     else:
-        # Sending path to simulator
+        # Parsing datas
         goal_str = goals_to_string(goal)
         robots_str = robots_coord_to_string(initial_state)
         path_str = path_to_string(
             path, get_robot_list(initial_state), width*height, width)
         message = build_message(file, goal_str, "", robots_str, path_str)
-        send_object(conf_list)
+        # Sending path to the simulator
         send_data(message)
         # send_data(message, IPAdr, port)
 
